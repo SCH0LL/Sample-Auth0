ebd36fbaafedb747133e0b69ba563f5f45a64996
hm-scholl <scholl@hm.edu>
2022-03-24 01:00:48 +0100

    First Commit Sample-Auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Imorted Auth0 Module from SDK
+import { AuthModule } from '@auth0/auth0-angular';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-4v8xa-a5.us.auth0.com',
+      clientId: '0boewlqJD188RGGvHP1CbyYCpOsuciHq'
+    }),
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+      {path: 'logout', component: LogoutComponent},
+      {path: '', redirectTo: '/login', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
