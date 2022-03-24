09811ce80432e381ec66082707c5f101ddfdbd0e
hm-scholl <scholl@hm.edu>
2022-03-24 08:59:48 +0100

    Material Design Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 // Imorted Auth0 Module from SDK
 import { AuthModule } from '@auth0/auth0-angular';
@@ -17,8 +21,12 @@ import { LogoutComponent } from './logout/logout.component';
   ],
   imports: [
     BrowserModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
     AppRoutingModule,
-    AuthModule.forRoot({
+        AuthModule.forRoot({
       domain: 'dev-4v8xa-a5.us.auth0.com',
       clientId: '0boewlqJD188RGGvHP1CbyYCpOsuciHq'
     }),
@@ -27,6 +35,12 @@ import { LogoutComponent } from './logout/logout.component';
       {path: 'logout', component: LogoutComponent},
       {path: '', redirectTo: '/login', pathMatch: 'full'}
     ])
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
